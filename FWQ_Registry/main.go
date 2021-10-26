b7aa9471608da2700f2899f1e491af7e2c61ed29
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-10-26 18:50:54 +0200

    Registry implementado

---
+++ FWQ_Registry/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	host = "localhost"
+	tipo = "tcp"
+)
+
+func main() {
+
+	puerto := os.Args[1]
+
+	fmt.Println("Arrancando el registrador atendiendo en " + host + ":" + puerto)
+
+	l, err := net.Listen(tipo, host+":"+puerto)
+
+	if err != nil {
+		fmt.Println("Error escuchando", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+
+	for {
+
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+		}
+
+		fmt.Println("Cliente " + c.RemoteAddr().String() + " conectado.")
+
+		go manejoConexion(c)
+
+	}
+
+}
+
+func manejoConexion(conexion net.Conn) {
+
+	buffer, err := bufio.NewReader(conexion).ReadBytes('\n')
+
+	if err != nil {
+		fmt.Println("Cliente desconectado.")
+		conexion.Close()
+		return
+	}
+
+	visitante := strings.Split(string(buffer[:len(buffer)-1]), ":")
+
+	log.Println("Visitante a registrar: " + visitante[0] + ":" + visitante[1] + ":" + visitante[2])
+
+	conexion.Write(buffer)
+
+	archivo, err := os.Create("./Visitante" + visitante[0] + ".txt")
+
+	if err != nil {
+		fmt.Println("Hubo un error")
+		return
+	}
+
+	fmt.Fprintln(archivo, "ID: "+visitante[0])
+	fmt.Fprintln(archivo, "Nombre: "+visitante[1])
+	fmt.Fprintln(archivo, "Password: "+visitante[2])
+
+	archivo.Close()
+
+	manejoConexion(conexion)
+
+}
