faaebc8ce683d00396648f971f2eca20f168f9c5
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-11-03 18:48:06 +0100

    Modificada la funcion enviaInformacion que ya no forma parte de la clase sensor

--- FWQ_Sensor/main.go
+++ FWQ_Sensor/main.go
@@ -21,36 +21,6 @@ type sensor struct {
 	Personas    int
 }
 
-/* Función que envía mediante un productor de Kafka la información recogida por el sensor  */
-func (this *sensor) enviaInformacion(ctx context.Context, brokerAddress string, tiempoAleatorio int) {
-
-	// Inicializamos el escritor
-	escritor := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{brokerAddress, timeServerAddress},
-		Topic:   topic,
-	})
-
-	for {
-
-		err := escritor.WriteMessages(ctx, kafka.Message{
-			Value: []byte("En la atracción " + strconv.Itoa(this.IdAtraccion) + " hay actualmente " + strconv.Itoa(this.Personas) + " personas"),
-		})
-		if err != nil {
-			panic("Error: No se puede escribir el mensaje: " + err.Error())
-		}
-
-		// Generamos un número aleatorio de personas
-		rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
-		min := 0
-		max := 10
-		this.Personas = (rand.Intn(max-min+1) + min)
-
-		// Cada x segundos el sensor envía la información al servidor de tiempos
-		time.Sleep(time.Duration(tiempoAleatorio) * time.Second)
-	}
-
-}
-
 func main() {
 
 	ipBrokerGestorColas := os.Args[1]
@@ -83,6 +53,36 @@ func main() {
 
 	// Envíamos al servidor de tiempos el número de personas que se encuentra en la cola de la atracción
 	ctx := context.Background()
-	s.enviaInformacion(ctx, brokerAddress, tiempoAleatorio)
+	enviaInformacion(s, ctx, brokerAddress, tiempoAleatorio)
+
+}
+
+/* Función que envía mediante un productor de Kafka la información recogida por el sensor  */
+func enviaInformacion(s *sensor, ctx context.Context, brokerAddress string, tiempoAleatorio int) {
+
+	// Inicializamos el escritor
+	escritor := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{brokerAddress, timeServerAddress},
+		Topic:   topic,
+	})
+
+	for {
+
+		err := escritor.WriteMessages(ctx, kafka.Message{
+			Value: []byte("En la atracción " + strconv.Itoa(s.IdAtraccion) + " hay actualmente " + strconv.Itoa(s.Personas) + " personas"),
+		})
+		if err != nil {
+			panic("Error: No se puede escribir el mensaje: " + err.Error())
+		}
+
+		// Generamos un número aleatorio de personas
+		rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+		min := 0
+		max := 10
+		s.Personas = (rand.Intn(max-min+1) + min)
+
+		// Cada x segundos el sensor envía la información al servidor de tiempos
+		time.Sleep(time.Duration(tiempoAleatorio) * time.Second)
+	}
 
 }
