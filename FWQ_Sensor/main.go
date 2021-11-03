4211f2bb0b7a5db3f00861929cf341845695d016
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-11-03 05:03:09 +0100

    Implementando sensores a falta de la funcion enviaInformacion

--- FWQ_Sensor/main.go
+++ FWQ_Sensor/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
+	"time"
 )
 
 const (
@@ -17,37 +19,50 @@ type sensor struct {
 	Personas    int
 }
 
-func main() {
-
-	var respuesta string
-
-	ipGestorColas := os.Args[1]
-	puertoGestorColas := os.Args[2]
-	idAtraccion := os.Args[3]
-
-	for respuesta != "no" {
-
-		fmt.Println("Desea añadir un sensor (si/no): ")
-		fmt.Scanln(&respuesta)
-
-		if respuesta == "si" {
-
-			creaSensor(ipGestorColas, puertoGestorColas, idAtraccion)
-
-		} else if respuesta == "no" {
-
-		} else {
-
-		}
-
-	}
+func (this *sensor) enviaInformacion(ip int, puerto int, id int) {
 
 }
 
-func creaSensor(ip, puerto, id string) {
+func main() {
 
+	// Nos guardamos los parámetros en formato entero
+	ipBrokerGestorColas, err := strconv.Atoi(os.Args[1])
+
+	if err != nil {
+		fmt.Println("Error: Introduzca por parámetros IP, PUERTO e ID")
+	}
+
+	puertoBrokerGestorColas, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		fmt.Println("Error: Introduzca por parámetros IP, PUERTO e ID")
+	}
+
+	idAtraccion, err := strconv.Atoi(os.Args[3])
+
+	if err != nil {
+		fmt.Println("Error: Introduzca por parámetros IP, PUERTO e ID")
+	}
+
+	// Creamos un sensor
 	s := new(sensor)
-	s.IdAtraccion = 
-	s.Personas = rand.Intn(10-0) + 0
+	s.IdAtraccion = idAtraccion
+	// Generamos un número aleatorio de personas
+	rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+	min := 0
+	max := 10
+	s.Personas = (rand.Intn(max-min+1) + min)
+	fmt.Printf("Sensor creado para la atracción %d en la que inicialmente hay %d personas\n", s.IdAtraccion, s.Personas)
+
+	// Generamos un tiempo aleatorio entre 1 y 3 segundos
+	rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+	min = 1
+	max = 3
+	tiempoAleatorio := (rand.Intn(max-min+1) + min)
+
+	// Cada x segundos el sensor envía la información al servidor de tiempos
+	intervalo := time.Tick(tiempoAleatorio * time.Second)
+
+	s.enviaInformacion()
 
 }
