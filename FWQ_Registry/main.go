8449d07da0f37fe2cf0336a4a2e0ae62a5c0d4bd
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-11-05 21:16:07 +0100

    Registry listo para añadir la parte de BD

--- FWQ_Registry/main.go
+++ FWQ_Registry/main.go
@@ -18,6 +18,7 @@ func main() {
 
 	puerto := os.Args[1]
 
+	// Arrancamos el servidor y atendemos conexiones entrantes
 	fmt.Println("Arrancando el registrador atendiendo en " + host + ":" + puerto)
 
 	l, err := net.Listen(tipo, host+":"+puerto)
@@ -27,17 +28,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Cerramos el listener cuando se cierra la aplicación
 	defer l.Close()
 
+	// Bucle infinito hasta la salida del programa
 	for {
 
+		// Atendemos conexiones entrantes
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error conectando:", err.Error())
 		}
 
+		// Imprimimos la dirección de conexión del cliente
 		fmt.Println("Cliente " + c.RemoteAddr().String() + " conectado.")
 
+		// Manejamos las conexiones de forma concurrente
 		go manejoConexion(c)
 
 	}
@@ -46,8 +52,10 @@ func main() {
 
 func manejoConexion(conexion net.Conn) {
 
+	// Lectura del buffer hasta el final de línea
 	buffer, err := bufio.NewReader(conexion).ReadBytes('\n')
 
+	// Cerramos la conexión de los clientes que se han desconectado
 	if err != nil {
 		fmt.Println("Cliente desconectado.")
 		conexion.Close()
@@ -56,11 +64,17 @@ func manejoConexion(conexion net.Conn) {
 
 	visitante := strings.Split(string(buffer[:len(buffer)-1]), " ")
 
-	log.Println("Visitante a registrar: " + visitante[0] + " | " + visitante[1] + " | " + visitante[2])
+	// Imprimimos la información del visitante a registrar o editar en la base de datos
+	log.Println("Visitante a registrar/editar: " + visitante[0] + " | " + visitante[1] + " | " + visitante[2])
 
+	// Mandamos un mensaje de respuesta al cliente
 	conexion.Write(buffer)
 
-	archivo, err := os.Create("./Visitante" + visitante[0] + ".txt")
+	// Reiniciamos el proceso
+	manejoConexion(conexion)
+
+	/* POR SI HICIERA FALTA HACERLO CON FICHEROS */
+	/*archivo, err := os.Create("./Visitante" + visitante[0] + ".txt")
 
 	if err != nil {
 		fmt.Println("Hubo un error")
@@ -71,8 +85,6 @@ func manejoConexion(conexion net.Conn) {
 	fmt.Fprintln(archivo, "Nombre: "+visitante[1])
 	fmt.Fprintln(archivo, "Password: "+visitante[2])
 
-	archivo.Close()
-
-	manejoConexion(conexion)
+	archivo.Close()*/
 
 }
