8d5d929c7ee1945ab0a9014c7619913587104d0d
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-11-06 21:35:41 +0100

    Renombrado de archivos y probando la conexion registry-visitantes

---
+++ FWQ_Registry/fwq_registry.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+/*
+* Estructura del visitante
+ */
+type visitante struct {
+	ID        string `json:"id"`
+	Nombre    string `json:"nombre"`
+	Password  string `json:"contraseña"`
+	Posicionx int    `json:"posicionx"`
+	Posiciony int    `json:"posiciony"`
+	Destinox  int    `json:"destinox"`
+	Destinoy  int    `json:"destinoy"`
+	Parque    string `json:"parqueAtracciones"`
+}
+
+const (
+	host = "localhost"
+	tipo = "tcp"
+)
+
+func main() {
+
+	puerto := os.Args[1]
+
+	// Arrancamos el servidor y atendemos conexiones entrantes
+	fmt.Println("Arrancando el registrador atendiendo en " + host + ":" + puerto)
+
+	l, err := net.Listen(tipo, host+":"+puerto)
+
+	if err != nil {
+		fmt.Println("Error escuchando", err.Error())
+		os.Exit(1)
+	}
+
+	// Cerramos el listener cuando se cierra la aplicación
+	defer l.Close()
+
+	// Bucle infinito hasta la salida del programa
+	for {
+
+		// Atendemos conexiones entrantes
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error conectando:", err.Error())
+		}
+
+		// Imprimimos la dirección de conexión del cliente
+		fmt.Println("Cliente " + c.RemoteAddr().String() + " conectado.")
+
+		// Manejamos las conexiones de forma concurrente
+		go manejoConexion(c)
+
+	}
+
+}
+
+func manejoConexion(conexion net.Conn) {
+
+	// Lectura del buffer hasta el final de línea
+	buffer, err := bufio.NewReader(conexion).ReadBytes('\n')
+
+	// Cerramos la conexión de los clientes que se han desconectado
+	if err != nil {
+		fmt.Println("Cliente desconectado.")
+		conexion.Close()
+		return
+	}
+
+	vis := strings.Split(string(buffer[:len(buffer)-1]), "|")
+
+	// Imprimimos la información del visitante a registrar o editar en la base de datos
+	log.Println("Visitante a registrar/editar: " + vis[0] + " | " + vis[1] + " | " + vis[2])
+
+	// Mandamos un mensaje de respuesta al cliente
+	conexion.Write(buffer)
+
+	// Accedemos a la base de datos, empezando por abrir la conexión
+	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/parque_atracciones")
+
+	// Comprobamos que no haya error al conectarse
+	if err != nil {
+		panic("Error al conectarse con la BD: " + err.Error())
+	}
+
+	defer db.Close() // Para que siempre se cierre la conexión con la BD al finalizar el programa
+
+	results, err := db.Query("SELECT * FROM VISITANTE WHERE id == " + vis[0])
+
+	// Comrpobamos que no se produzcan errores al hacer la consulta
+	if err != nil {
+		panic("Error al hacer la consulta a la BD: " + err.Error())
+	}
+
+	v := visitante{
+		ID:       vis[0],
+		Nombre:   vis[1],
+		Password: vis[2],
+	}
+
+	// Comprobamos que la consulta haya devuelto alguna fila de la BD
+	// Si el visitante existe en la BD
+	if results.Next() {
+
+		// MODIFICAMOS la información de dicho visitante en la BD
+		// Preparamos para prevenir inyecciones SQL
+		sentenciaPreparada, err := db.Prepare("UPDATE visitante SET id = ?, nombre = ?, contraseña = ? WHERE id == " + vis[0])
+		if err != nil {
+			panic("Error al preparar la sentencia de modificación: " + err.Error())
+		}
+
+		defer sentenciaPreparada.Close()
+
+		// Ejecutar sentencia, un valor por cada '?'
+		_, err = sentenciaPreparada.Exec(v.ID, v.Nombre, v.Password)
+		if err != nil {
+			panic("Error al modificar el visitante: " + err.Error())
+		}
+
+		fmt.Println("Visitante modificado.")
+
+	} else { // Sino existe en la BD
+
+		// INSERTAMOS el nuevo visitante en la BD
+		// Preparamos para prevenir inyecciones SQL
+		sentenciaPreparada, err := db.Prepare("INSERT INTO visitante (id, nombre, contraseña) VALUES(?, ?, ?)")
+		if err != nil {
+			panic("Error al preparar la sentencia de inserción: " + err.Error())
+		}
+
+		defer sentenciaPreparada.Close()
+
+		// Ejecutar sentencia, un valor por cada '?'
+		_, err = sentenciaPreparada.Exec(v.ID, v.Nombre, v.Password)
+		if err != nil {
+			panic("Error al modificar el visitante: " + err.Error())
+		}
+
+		fmt.Println("Visitante registrado en el parque.")
+
+	}
+
+	// Reiniciamos el proceso
+	manejoConexion(conexion)
+
+	/* POR SI HICIERA FALTA HACERLO CON FICHEROS */
+	/*archivo, err := os.Create("./Visitante" + visitante[0] + ".txt")
+
+	if err != nil {
+		fmt.Println("Hubo un error")
+		return
+	}
+
+	fmt.Fprintln(archivo, "ID: "+visitante[0])
+	fmt.Fprintln(archivo, "Nombre: "+visitante[1])
+	fmt.Fprintln(archivo, "Password: "+visitante[2])
+
+	archivo.Close()*/
+
+}
---
+++ FWQ_Sensor/fwq_sensor.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type sensor struct {
+	IdAtraccion int
+	Personas    int
+}
+
+func main() {
+
+	ipBrokerGestorColas := os.Args[1]
+
+	puertoBrokerGestorColas := os.Args[2]
+
+	idAtraccion, err := strconv.Atoi(os.Args[3])
+
+	if err != nil {
+		panic("Error: Introduzca por parámetros IP, PUERTO e ID " + err.Error())
+	}
+
+	brokerAddress := ipBrokerGestorColas + ":" + puertoBrokerGestorColas
+
+	// Creamos un sensor
+	s := new(sensor)
+	s.IdAtraccion = idAtraccion
+	// Generamos un número aleatorio de personas
+	rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+	min := 0
+	max := 10
+	s.Personas = (rand.Intn(max-min+1) + min)
+	fmt.Printf("Sensor creado para la atracción %d en la que inicialmente hay %d personas\n", s.IdAtraccion, s.Personas)
+
+	// Generamos un tiempo aleatorio entre 1 y 3 segundos
+	rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+	min = 1
+	max = 3
+	tiempoAleatorio := (rand.Intn(max-min+1) + min)
+
+	// Envíamos al servidor de tiempos el número de personas que se encuentra en la cola de la atracción
+	enviaInformacion(s, brokerAddress, tiempoAleatorio)
+
+}
+
+/* Función que envía mediante un productor de Kafka la información recogida por el sensor  */
+func enviaInformacion(s *sensor, brokerAddress string, tiempoAleatorio int) {
+
+	// Inicializamos el escritor
+	escritor := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9094"},
+		Topic:   "sd-events",
+	})
+
+	for {
+
+		err := escritor.WriteMessages(context.Background(),
+			kafka.Message{
+				Key:   []byte("Atraccion " + strconv.Itoa(s.IdAtraccion)),
+				Value: []byte(strconv.Itoa(s.IdAtraccion) + ":" + strconv.Itoa(s.Personas)),
+			})
+
+		if err != nil {
+			panic("Error: No se puede escribir el mensaje: " + err.Error())
+		}
+
+		// Generamos un número aleatorio de personas
+		rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
+		min := 0
+		max := 10
+		s.Personas = (rand.Intn(max-min+1) + min)
+
+		// Cada x segundos el sensor envía la información al servidor de tiempos
+		time.Sleep(time.Duration(tiempoAleatorio) * time.Second)
+	}
+
+}
