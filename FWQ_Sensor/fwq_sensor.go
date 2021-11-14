f910ce475680e38b1f6189eee4adcbe35342858f
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2021-11-14 03:51:10 +0100

    Aumentado el número posible de personas en cola a 40

--- FWQ_Sensor/fwq_sensor.go
+++ FWQ_Sensor/fwq_sensor.go
@@ -33,7 +33,7 @@ func main() {
 	// Generamos un número aleatorio de personas inicialmente en la cola de la atracción
 	rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
 	min := 0
-	max := 10
+	max := 40
 	s.Personas = (rand.Intn(max-min+1) + min)
 	fmt.Println("Sensor creado para la atracción (" + idAtraccion + ") en la que inicialmente hay " + strconv.Itoa(s.Personas) + " en cola")
 
@@ -72,7 +72,7 @@ func enviaInformacion(s *sensor, brokerAddress string, tiempoAleatorio int) {
 		// Generamos un número aleatorio de personas en cola
 		rand.Seed(time.Now().UnixNano()) // Utilizamos la función Seed(semilla) para inicializar la fuente predeterminada al requerir un comportamiento diferente para cada ejecución
 		min := 0
-		max := 10
+		max := 40
 		s.Personas = (rand.Intn(max-min+1) + min)
 
 		// Cada x segundos el sensor envía la información al servidor de tiempos
