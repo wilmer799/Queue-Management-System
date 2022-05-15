33584c88fe69f3978a4acc6cb3018b6e7777e53b
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-05-16 00:38:47 +0200

    Controlada la caída de los sensores respecto al engine

--- FWQ_WaitingTimeServer/fwq_waitingTimeServer.go
+++ FWQ_WaitingTimeServer/fwq_waitingTimeServer.go
@@ -30,6 +30,8 @@ type atraccion struct {
 	Parque       string `json:"parqueAtracciones"`
 }
 
+var atracciones []atraccion // Variable global que irá almacenando la información actual de las atracciones del parque
+
 func main() {
 
 	host := os.Args[1]
@@ -52,7 +54,9 @@ func main() {
 	// Cerramos el listener cuando se cierra la aplicación
 	defer l.Close()
 
-	// Bucle infinito hasta la salida del programa
+	go recibirInfoSensores(ipBrokerGestorColas, puertoBrokerGestorColas)
+
+	// Bucle infinito hasta la salida del programa en el que se tratan las peticiones recibidas por el engine
 	for {
 
 		// Atendemos conexiones entrantes
@@ -65,8 +69,55 @@ func main() {
 		log.Println("Cliente engine " + c.RemoteAddr().String() + " conectado.")
 
 		// Manejamos las conexiones de forma concurrente
-		go manejoConexion(ipBrokerGestorColas, puertoBrokerGestorColas, c)
+		go manejoConexion(c)
 
+	}
+
+}
+
+/* Función que se encarga de recibir el número de personas que hay en las colas por parte de los sensores */
+func recibirInfoSensores(IpBroker, PuertoBroker string) {
+
+	broker := IpBroker + ":" + PuertoBroker
+	r := kafka.ReaderConfig(kafka.ReaderConfig{
+		Brokers:     []string{broker},
+		Topic:       "sensor-servidorTiempos",
+		GroupID:     Ulid(),
+		StartOffset: kafka.LastOffset,
+	})
+
+	reader := kafka.NewReader(r)
+
+	for {
+
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("Ha ocurrido algún error a la hora de conectarse con kafka", err)
+		}
+
+		if strings.Contains(string(m.Value), "desconectado") {
+			fmt.Println(string(m.Value))
+		} else {
+			fmt.Println("[", string(m.Value)+" personas en cola", "]")
+
+			infoSensor := strings.Split(string(m.Value), ":")
+
+			idAtraccion := infoSensor[0]
+			personasEnCola, _ := strconv.Atoi(infoSensor[1])
+
+			encontrado := false
+
+			// Buscamos la atracción indicada por el sensor para calcular su tiempo de espera actual
+			for i := 0; i < len(atracciones) && !encontrado; i++ {
+
+				if atracciones[i].ID == idAtraccion {
+					encontrado = true
+					atracciones[i].TiempoEspera = calculaTiempoEspera(atracciones[i], personasEnCola)
+				}
+
+			}
+
+		}
 	}
 
 }
@@ -89,7 +140,7 @@ func calculaTiempoEspera(a atraccion, personasEnCola int) int {
 }
 
 // Función que maneja la lógica para una única petición de conexión
-func manejoConexion(IpBroker, PuertoBroker string, conn net.Conn) {
+func manejoConexion(conn net.Conn) {
 
 	// Lectura del buffer de entrada hasta el final de línea
 	buffer, err := bufio.NewReader(conn).ReadBytes('\n')
@@ -98,13 +149,11 @@ func manejoConexion(IpBroker, PuertoBroker string, conn net.Conn) {
 
 	infoAtracciones := strings.Split(string(buffer), "|")
 
-	var atracciones []atraccion
-
 	// El -1 es porque la longitud del array es 17, ya que al terminar la cadena en la barra vertical
 	// el split realizado coge un último elemento para formar el array que contiene un espacio en blanco.
 	for i := 0; i < (len(infoAtracciones) - 1); i++ {
 
-		//fmt.Println(infoAtracciones[i])
+		fmt.Println(infoAtracciones[i])
 		infoAtraccion := strings.Split(infoAtracciones[i], ":")
 
 		var a = atraccion{
@@ -122,11 +171,22 @@ func manejoConexion(IpBroker, PuertoBroker string, conn net.Conn) {
 		tiempoEspera, _ := strconv.Atoi(infoAtraccion[3])
 		a.TiempoEspera = tiempoEspera
 
-		atracciones = append(atracciones, a)
+		encontrada := false
+
+		// Si la atracción ya la tenemos almacenada, actualizamos su información y sino la añadimos al slice
+		for i := 0; i < len(atracciones) && !encontrada; i++ {
+			if atracciones[i].ID == a.ID {
+				encontrada = true
+			}
+		}
+
+		if !encontrada {
+			atracciones = append(atracciones, a)
+		}
 
 	}
 
-	//fmt.Println("Longitud atracciones: " + strconv.Itoa(len(atracciones)))
+	fmt.Println("Longitud atracciones: " + strconv.Itoa(len(atracciones)))
 
 	// Cerrar las conexiones con engines desconectados
 	if err != nil {
@@ -135,73 +195,19 @@ func manejoConexion(IpBroker, PuertoBroker string, conn net.Conn) {
 		return
 	}
 
-	broker := IpBroker + ":" + PuertoBroker
-	r := kafka.ReaderConfig(kafka.ReaderConfig{
-		Brokers:     []string{broker},
-		Topic:       "sensor-servidorTiempos",
-		GroupID:     Ulid(),
-		StartOffset: kafka.LastOffset,
-	})
+	// Enviamos al engine los tiempos de espera actuales
+	tiemposEspera := ""
 
-	reader := kafka.NewReader(r)
-
-	for {
-
-		m, err := reader.ReadMessage(context.Background())
-		if err != nil {
-			fmt.Println("Ha ocurrido algún error a la hora de conectarse con kafka", err)
-		}
-
-		if strings.Contains(string(m.Value), "desconectado") {
-
-			fmt.Println(string(m.Value))
-
-			// Enviamos al engine los tiempos de espera actuales
-			tiemposEspera := ""
-
-			// Formamos la cadena con los tiempos de espera que le vamos a mandar al engine
-			for i := 0; i < len(atracciones); i++ {
-				tiemposEspera += atracciones[i].ID + ":" + strconv.Itoa(atracciones[i].TiempoEspera) + "|"
-			}
-
-			// Mandamos una cadena separada por barras con los tiempos de espera de cada atracción al engine
-			conn.Write([]byte(tiemposEspera))
-			conn.Close() // Cerramos la conexión con el engine
-
-		} else {
-			fmt.Println("[", string(m.Value)+" personas en cola", "]")
-
-			infoSensor := strings.Split(string(m.Value), ":")
-
-			idAtraccion := infoSensor[0]
-			personasEnCola, _ := strconv.Atoi(infoSensor[1])
-
-			encontrado := false
-
-			// Buscamos la atracción indicada por el sensor para calcular su tiempo de espera actual
-			for i := 0; i < len(atracciones) && !encontrado; i++ {
-
-				if atracciones[i].ID == idAtraccion {
-					encontrado = true
-					atracciones[i].TiempoEspera = calculaTiempoEspera(atracciones[i], personasEnCola)
-				}
-
-			}
-
-			tiemposEspera := ""
-
-			// Formamos la cadena con los tiempos de espera que le vamos a mandar al engine
-			for i := 0; i < len(atracciones); i++ {
-				tiemposEspera += atracciones[i].ID + ":" + strconv.Itoa(atracciones[i].TiempoEspera) + "|"
-			}
-
-			// Mandamos una cadena separada por barras con los tiempos de espera de cada atracción al engine
-			conn.Write([]byte(tiemposEspera))
-			conn.Close()
-
-		}
-
+	// Formamos la cadena con los tiempos de espera que le vamos a mandar al engine
+	for i := 0; i < len(atracciones); i++ {
+		tiemposEspera += atracciones[i].ID + ":" + strconv.Itoa(atracciones[i].TiempoEspera) + "|"
 	}
+
+	// Mandamos una cadena separada por barras con los tiempos de espera de cada atracción al engine
+	conn.Write([]byte(tiemposEspera))
+	conn.Close() // Cerramos la conexión con el engine
+
+	//manejoConexion(conn)
 
 }
 
